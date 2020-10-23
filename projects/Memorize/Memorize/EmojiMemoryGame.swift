1f0801e987a1624f787312d7b756586d379f3271
Mohamed Mostafa Fawzi <mohamed.mostafa.fawzi@hotmail.com>
2020-10-23 05:47:22 +0200

    Submit Assignment I: Memorize

---
+++ projects/Memorize/Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,33 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Mohamed Canaria on 10/23/20.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> = createMemoryGame()
+        
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis: Array<String> = ["👻", "🎃", "🕷", "👺", "👹"]
+        let random = Int.random(in: 1..<emojis.count)
+        return MemoryGame<String>(numberOfPairsOfCards: random) { pairIndex in
+            
+            emojis[pairIndex]
+        }
+    }
+    
+    // MARK:- Access to the Model
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+    // MARK: - Intent(s)
+    
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
---
+++ projects/Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,18 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Mohamed Canaria on 10/20/20.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            let game = EmojiMemoryGame()
+            ContentView(viewModel: game)
+        }
+    }
+}
